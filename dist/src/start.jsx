1fae9cac7df707e77a7a6090b2b6920b9957e621
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-22 15:52:03 +0800

    add deploy files

---
+++ dist/src/start.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TicTacToe from './Tic Tac Toe/TicTacToe';
+
+export default function Start() {
+    document.title = 'Tik Tac Toe' // document title for this project
+
+    return (
+        <TicTacToe /> // Project
+    )
+}
+// this will serve as the APP.JS
