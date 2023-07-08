5bf4bbdd50a233cd2bb9ac4b1824a3e6b031d02b
Renstrio24p <Renstrio24p@users.noreply.github.com>
2023-07-08 01:14:07 +0000

    deploy: 0f4bdcdb80491e1be4427429d7fa81449a0b909f

--- src/components/DecisionMade.tsx
+++ src/components/DecisionMade.tsx
@@ -1,24 +1,26 @@
-export default function DecisionWinner(setsquare: any){
-
+export default function DecisionWinner(setsquare: any) {
     const line = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-    
-    for (let i = 0; i < line.length; i++){
-        const [a , b , c] = line[i]
-        if(
-            setsquare[a] && setsquare[a] === 
-            setsquare[b] && setsquare[a] ===
-            setsquare[c]){
-            return setsquare[a]
-        } 
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+  
+    for (let i = 0; i < line.length; i++) {
+      const [a, b, c] = line[i];
+      if (
+        setsquare[a] &&
+        setsquare[a] === setsquare[b] &&
+        setsquare[a] === setsquare[c]
+      ) {
+        return setsquare[a];
+      }
     }
+  
     return null;
-}
+  }
+  
